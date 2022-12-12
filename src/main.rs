fea808e6dde986eea0d443aa5927eef844d73e0b
Yatender Singh <yatender.nitk@gmail.com>
2022-12-12 20:08:28 +0100

    using guards

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-use actix_web::{get, web, App, HttpServer};
-use std::sync::Mutex;
-struct AppStateWithCounter {
-    counter: Mutex<i32>,
-}
+use actix_web::{get, web, App, HttpServer, HttpResponse, guard};
 
 #[get("/")]
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-
-    format!("Request number, {counter}")
+async fn index() -> String {
+    format!("Request number")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-
-    HttpServer::new(move || App::new().app_data(counter.clone()).service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+        .service(
+            web::scope("/")
+            .guard(guard::Header("Host", "www.rust-lang.org"))
+            .route("", web::to(|| async {HttpResponse::Ok().body("www")})),
+        )
+        .service(
+            web::scope("/")
+            .guard(guard::Header("Host", "users.rust-lang.org"))
+            .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
+        )
+        .route("/", web::to(HttpResponse::Ok))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
