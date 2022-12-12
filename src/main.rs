18dc6b41f7fccb23304e5621b436a63ce88afb7c
Yatender Singh <yatender.nitk@gmail.com>
2022-12-12 20:26:17 +0100

    - service config

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use actix_web::{get, web, App, HttpServer, HttpResponse, guard};
+use actix_web::{get, web, App, HttpServer, HttpResponse};
 
 #[get("/")]
 async fn index() -> String {
     format!("Request number")
 }
 
+
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+        .route(web::get().to(|| async {HttpResponse::Ok().body("test")}))
+        .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+        .route(web::get().to(|| async {HttpResponse::Ok().body("app")}))
+        .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-        .service(
-            web::scope("/")
-            .guard(guard::Header("Host", "www.rust-lang.org"))
-            .route("", web::to(|| async {HttpResponse::Ok().body("www")})),
+        .configure(config)
+        .service(web::scope("/api")).configure(scoped_config)
+        .route(
+            "/",
+            web::get().to(|| async { HttpResponse::Ok().body("/") }),
         )
-        .service(
-            web::scope("/")
-            .guard(guard::Header("Host", "users.rust-lang.org"))
-            .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
-        )
-        .route("/", web::to(HttpResponse::Ok))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
