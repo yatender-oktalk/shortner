14198dc212cb9b33e9181c1d5b8a73377123eeb5
Yatender Singh <yatender.nitk@gmail.com>
2022-12-14 20:57:26 +0100

    Logging and default hearders

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use actix_web::{
-    get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result
+    get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+    middleware::Logger,
+    middleware
 };
 use serde::{Deserialize, Serialize};
+use env_logger::Env;
+
 #[derive(Deserialize)]
 struct Info {
     username: String,
@@ -44,7 +48,7 @@ async fn create_user(req: web::Json<UserSignUpRequest>) -> HttpResponse {
         name: info.email,
     });
 
-    HttpResponse::Ok()
+    HttpResponse::Created()
     .content_type("application/json")
     .insert_header(("auth-token", "TYUIO98HJIKDKJDLFJOSOJUI"))
     .json(create_user_resp)
@@ -64,8 +68,14 @@ async fn show_all_users() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
-        App::new().service(
+        App::new()
+        .wrap(Logger::default())
+            .wrap(Logger::new("%a %s %b %{User-Agent}i %T"))
+            .wrap(middleware::DefaultHeaders::new().add(("X-Version", "0.2")))
+            .service(
             web::scope("/users")
                 .service(create_user)
                 .service(show_user)
